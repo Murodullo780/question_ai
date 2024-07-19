7877751f592ff0299a7f306d060aead23541fbb8
murodulloh <murodilla780@gmail.com>
2024-07-19 17:21:49 +0500

    fixed bugs by keyboard, internet

--- lib/question_ai_project.dart
+++ lib/question_ai_project.dart
@@ -7,6 +7,7 @@ class QuestionAiProject extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      resizeToAvoidBottomInset: false,
       body: QuizScreen(),
     );
   }
